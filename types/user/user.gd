7fb54d8f0a3517924272951ac3e0b1cd214068c5
tuzhanhong <tuzhanhong@corp.netease.com>
2023-11-20 21:11:23 +0800

    add:技能系统&玩家体力状态

--- types/user/user.gd
+++ types/user/user.gd
@@ -12,19 +12,23 @@ var hp: int : set = hp_change
 var mp: int : set = mp_change
 var money: int : set = money_change
 
-var runable: bool = true
 var is_control: bool = false
 var is_follow: bool = false
+var is_tired: bool = false
+var is_coma: bool = false
 # 保持动画不被打断
 var stand_animation: bool = false
 var eat_times = 0
 
 var screen_size
 var follow_player
+var skills = SkillTree.new()
 
 enum Player {
 	DIANA, TRAVERLER
 }
+
+var dialog_head = preload("res://types/common/dialog_head.tscn")
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -50,20 +54,16 @@ func _ready():
 	
 
 func _process(delta):
+	# 动画未结束不能进行操作
+	if stand_animation:
+		return
+	# 昏倒后不能进行任何操作
+	if is_coma:
+		return
 	# 不是主控对象
 	if !is_control:
 		if is_follow:
 			follow(delta)
-		return
-	# 动画未结束不能进行操作
-	if stand_animation:
-		return
-	if !runable:
-		$animation.play(get_play_animation("exhausted"))
-#		hp = clamp(hp + hp_recover, 0, hp_max)
-		if(hp == hp_max):
-			runable = true
-			$animation.play(get_play_animation("stand"))
 		return
 	var animation = get_play_animation("stand")
 	velocity = Vector2(0,0)
@@ -79,11 +79,13 @@ func _process(delta):
 		velocity.y -= 1
 
 	if velocity.length() > 0:
-		if Input.is_action_pressed("run"):
+		# 精疲力尽不能奔跑
+		if Input.is_action_pressed("run") and !is_tired:
 			velocity = velocity.normalized() * run_speed
+			$animation.play(get_play_animation("run"))
 		else:
 			velocity = velocity.normalized() * speed
-		$animation.play(get_play_animation("walk"))
+			$animation.play(get_play_animation("walk"))
 	else:
 		$animation.play(get_play_animation("stand"))
 	move_and_slide()
@@ -110,6 +112,14 @@ func hp_change(value):
 func mp_change(value):
 	mp = value
 	ui.get_node(get_player_status("mp")).value = mp
+	if mp < 0 and !is_tired:
+		switch_tired(true)
+	if mp > 0 and is_tired:
+		switch_tired(false)
+	if mp < -15 and !is_coma:
+		switch_coma(true)
+	if mp > -15 and is_coma:
+		switch_coma(false)
 	print(str(player) + " mp_change " + str(ui.get_node(get_player_status("mp")).value))
 
 func hp_max_change(value):
@@ -142,14 +152,13 @@ func use(use_item):
 			eat_times += 1
 		if item.hp != 0:
 			# 该道具有回复HP能力
-			hp = clamp(hp + item.hp, 0, hp_max)
+			hp = clamp(hp + item.hp, -100, hp_max)
 		if item.mp != 0:
 			# 该道具有回复MP能力
-			mp = clamp(mp + item.mp, 0, mp_max)
+			mp = clamp(mp + item.mp, -100, mp_max)
 		if item.tool != null:
 			# 该道具为可以使用的工具
 			item.tool.use()
-			mp -= 10
 		if item.blind_box != null:
 			# 该道具为盲盒
 			var gifts = item.blind_box.open()
@@ -201,36 +210,28 @@ func follow(delta):
 
 # 使用锄头
 func _on_using_hoe():
-	# 从配置中获取
-	var mp_consume = 10
-	if is_control and player == Player.TRAVERLER and mp > mp_consume:
-		stand_animation = true
-		$animation.play(get_play_animation("hoeing"))
-		mp = clamp(mp - mp_consume, 0, mp_max)
+	_use_tool("hoe")
 
 # 使用斧头
 func _on_using_axe():
-	var mp_consume = 10
-	if is_control and player == Player.TRAVERLER and mp > mp_consume:
-		stand_animation = true
-		$animation.play(get_play_animation("chop"))
-		mp = clamp(mp - mp_consume, 0, mp_max)
+	_use_tool("axe")
 
 # 使用棍棒
 func _on_using_stick():
-	var mp_consume = 10
-	if is_control and player == Player.TRAVERLER and mp > mp_consume:
-		stand_animation = true
-		$animation.play(get_play_animation("stick"))
-		mp = clamp(mp - mp_consume, 0, mp_max)
+	_use_tool("stick")
 
 # 使用镐子
 func _on_using_pickaxe():
-	var mp_consume = 10
-	if is_control and player == Player.TRAVERLER and mp > mp_consume:
+	_use_tool("pickaxe")
+
+# 使用工具
+func _use_tool(tool_name: String):
+	var mp_consume = skills.get_consume(tool_name)
+	if is_control and player == Player.TRAVERLER:
 		stand_animation = true
-		$animation.play(get_play_animation("tap"))
-		mp = clamp(mp - mp_consume, 0, mp_max)
+		$animation.play(get_play_animation(tool_name))
+		mp = clamp(mp - mp_consume, -100, mp_max)
+		skills.improve_skill(tool_name)
 
 # 订阅Diana的信号
 func diana_connect():
@@ -258,3 +259,42 @@ func switch_status():
 			ui.get_node("status/traveler").show()
 		else:
 			ui.get_node("status/traveler").hide()
+
+func switch_tired(flag: bool):
+	if flag:
+		is_tired = true
+		dialog_head = dialog_head.instantiate()
+		dialog_head.text = "X"
+		dialog_head.position = Vector2(-40,-90)
+		add_child(dialog_head)
+	else :
+		is_tired = false
+
+func switch_coma(flag: bool):
+	if flag:
+		is_coma = true
+		# 双方都晕倒了
+		if _get_another_player().is_coma:
+			# todo 损失金钱 + 强行睡觉
+			pass
+		else :
+			# todo 播放昏迷动画
+			$animation.play(get_play_animation("stand"))
+			change_control_player()
+			_get_another_player().change_control_player()
+	else :
+		is_coma = false
+
+func _get_another_player():
+	if player == Player.DIANA:
+		return traveler
+	else:
+		return diana
+
+func sleep():
+	# todo 移动到床上
+	var recover_mp = mp_max
+	if is_tired or is_coma:
+		recover_mp = recover_mp * 0.5
+	mp = clamp(mp + recover_mp, -100, mp_max)
+	
